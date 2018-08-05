b5ed06689f9272b9e3d4352d59bde51f3b79a60d
Henry Lorenzo <hlorenzo93@hotmail.com>
2018-08-05 13:35:47 -0400

    Checkbox only choose one

--- register.php
+++ register.php
@@ -1,9 +1,11 @@
+<!-- In the register.php is define all things for the register page -->
 <?php include('server.php'); ?>
 <!DOCTYPE html>
 <html>
     <head>
         <title>Registrar usuario prestamista o prestatario</title>    
         <link rel="stylesheet" type="text/css" href="style.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     </head>    
     <body>
         <div class="header">
@@ -32,9 +34,24 @@
             <div class="input-group">
                 <button type="submit" name="register" class="btn">Enviar</button>
             </div>
+
+            <div class="input-group">
+                <input type="checkbox" name="skill" value="prestatario"> Prestatario</input>
+                <input type="checkbox" name="skill" value="Prestamista"> Prestamista</input>
+            </div>
             <p>
                 ¿Ya tienes una cuenta? <a href="login.php">Ingresar</a>
             </p>
+
+            <!-- this is the javascritp code 
+                for the only choose one checkbox for -->
+                <script>
+                $(document).ready(function(){
+                    $('.check').click(function() {
+                        $('.check').not(this).prop('checked', false);
+                        });
+                    });
+                </script>
         </form>
     </body>
 </html>
